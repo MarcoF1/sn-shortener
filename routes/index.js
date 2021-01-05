bf7744942ee2b0f81a89ea5d7fd0a3724965f137
MarcoF1 <marco1fleming@gmail.com>
2021-01-05 17:26:26 -0600

    short url redirects

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,58 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const Shorts = require("../models/Shorts");
+const v = require("./validators");
+
+const router = express.Router();
+
+/**
+ * Serves homepage.
+ * @name GET /
+ */
+router.get("/", [], async (req, res) => {
+  res.redirect("https://sigmanu.mit.edu");
+});
+
+/**
+ * Access short URL.
+ *
+ * Note that the ? after shortName makes the shotName param
+ * accessible from within this route **even when it is empty**
+ * which makes sure that do hit this route
+ *
+ * @name GET /:name
+ */
+router.get("/:name?", [v.ensureValidShortNameParam], async (req, res, next) => {
+  try {
+    // middleware will check that the param is non-empty!
+    const shortName = req.params.name;
+
+    // fetch short from DB and ensure it exists
+    const short = await Shorts.findOne(shortName);
+
+    if (!short) {
+      res
+        .status(404)
+        .json({
+          error: `Short URL ${shortName} not found.`,
+        })
+        .end();
+      return;
+    }
+
+    // if the destination URL of the short is specified,
+    // then navigate to it by redirecting
+    if (short.url) {
+      res.redirect(short.url);
+    } else {
+      next();
+    }
+  } catch (error) {
+    res
+      .status(503)
+      .json({ error: "Could not navigate to desination URL of this short" })
+      .end();
+  }
 });
 
 module.exports = router;
