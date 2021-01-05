67caab5f6db6cb14062eef696bd6733491087999
MarcoF1 <marco1fleming@gmail.com>
2021-01-05 16:07:07 -0600

    restarting from express boilerplate

--- app.js
+++ app.js
@@ -1,57 +1,41 @@
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const session = require("express-session");
-const cors = require("cors");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-// import all the express routes we will be using
-const indexRouter = require("./routes/index");
-const shortsRouter = require("./routes/shorts");
-const usersRouter = require("./routes/users");
-const sessionRouter = require("./routes/session");
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-// create our app
-const app = express();
+var app = express();
 
-// set up user session
-app.use(
-  session({
-    secret: "URL-shortener",
-    resave: true,
-    saveUninitialized: true,
-  })
-);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-// allows us to make requests from POSTMAN
-app.use(cors());
-
-// set up the app to use dev logger
-app.use(logger("dev"));
-
-// accept json
+app.use(logger('dev'));
 app.use(express.json());
-
-// https://stackoverflow.com/questions/29960764/what-does-extended-mean-in-express-4-0
-// allows object nesting in POST
 app.use(express.urlencoded({ extended: false }));
-
-// cookies for sessions
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// server html+css+js frontend
-app.use(express.static(path.join(__dirname, "dist")));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-// connect url hierarchies to our routers
-app.use("/", indexRouter);
-app.use("/api/shorts", shortsRouter);
-app.use("/api/users", usersRouter);
-app.use("/api/users/session", sessionRouter);
-
-app.use("*", function(req, res) {
-  res.redirect("/").end();
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-console.log("Running on localhost:3000...");
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
